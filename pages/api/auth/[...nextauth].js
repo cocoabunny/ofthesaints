01c6e54489702b23453cb37678d663f0974165fe
cocoabunny <65260797+cocoabunny@users.noreply.github.com>
2022-10-06 21:08:21 +1100

    integrating prisma

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,19 +1,22 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import GitHubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-      GoogleProvider ({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      }),
-      GitHubProvider ({
-        clientId: process.env.GITHUB_ID,
-        clientSecret: process.env.GITHUB_SECRET,
-      }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
     // ...add more providers here
   ],
-  secret: process.env.JWT_SECRET
+  // secret: process.env.JWT_SECRET
+  //   callbacks: {
+  //     async
+  //   }
 });
