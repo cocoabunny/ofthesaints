50b635f273e5091900179b6eb628f95b9b5c4718
cocoabunny <65260797+cocoabunny@users.noreply.github.com>
2022-10-10 22:13:26 +1100

    :tada: added prisma to proj

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,10 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
+import { PrismaClient } from "@prisma/client";
+import { signIn } from "next-auth/react";
+
+const prisma = new PrismaClient();
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -15,6 +19,23 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  events: {
+    async signIn(message) {
+      console.log(message);
+      await prisma.user.upsert({
+        where: {
+          email: message.profile.email,
+        },
+        update: {
+          name: message.profile.name,
+        },
+        create: {
+          email: message.profile.email,
+          name: message.profile.name,
+        },
+      });
+    },
+  },
   // secret: process.env.JWT_SECRET
   //   callbacks: {
   //     async
