8ad9285276c0f69216122be9c116e4ef3acb4aca
cocoabunny <65260797+cocoabunny@users.noreply.github.com>
2022-11-29 20:13:58 +1100

    Conditional rendering working WOOOO

--- components/ConditionalTest.jsx
+++ components/ConditionalTest.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { useSession } from "next-auth/react";
 
 const conditionalTest = () => {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
 
-  if (session === "Authenticated") {
+  if (status === "authenticated") {
     return <h1>Authenticated Page</h1>;
+  } else {
+    return <h1>Un-Authenticated Page</h1>;
   }
-  return <h1>Un-Authenticated Page</h1>;
 };
 
 export default conditionalTest;
