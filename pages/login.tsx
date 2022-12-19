78b6bea49271fd38b321e3eca5b5c50059cf9c14
cocoabunny <65260797+cocoabunny@users.noreply.github.com>
2022-12-19 14:34:13 +1100

    git push

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,16 +1,30 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 import Image from "next/image";
+import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const LoginPage = () => {
   const { data: session } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 5000);
+  }, [])
+
 
   if (session && session.user) {
     return (
-      <div>
-        <p>
-          Welcome {session.user.name}
-        </p>
-{/* <Image src={session.user.image} alt="lol" layout="fill"/> */}
+      <div className="bg-[#4B584B] bg-cover">
+        <div>
+          <h1>Welcome {session.user.name}</h1>
+          <h2>follow the rabbit trail for a surprise...</h2>
+        </div>
+          
+        
+
 
         {/* <Image
           src={session.user.image}
@@ -26,14 +40,14 @@ const LoginPage = () => {
   }
 
   return (
-    <>
+    <div className="h-full w-full bg-gradient-to-br from-[#DDD6A8] to-[#ECA3AA]">
       <p>
         Currently you are not signed, in.
         <br />
         Would you like to change that?
       </p>
       <button className="bg-red-300" onClick={() => signIn()}>Sign in</button>
-    </>
+      </div>
   );
 };
 
