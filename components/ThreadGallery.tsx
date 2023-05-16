610a94d8a31d4c4e9df89473ae1ea2a53e9a7e90
cocoabunny <65260797+cocoabunny@users.noreply.github.com>
2023-05-16 12:08:10 +1000

    found the syntax error, reworking gallery atm

--- components/ThreadGallery.tsx
+++ components/ThreadGallery.tsx
@@ -4,15 +4,17 @@ import testPicture from "../public/assets/gallery/Alpine Alpha-173.jpg";
 
 const threadGallery = () => {
   return (
-<>
-    <h1>TItle</h1>
-    <div className="w-full h-full grid grid-cols-2 md:grid-cols-3 gap-5 px-10">
+    <>
+
+<div className="w-full h-full grid grid-cols-2 md:grid-cols-3 gap-5 px-10">
     <div className="grid gap-4">
         <div className="relative h-auto max-w-full">
         <Image src={testPicture} alt="a test" className="h-auto max-w-full rounded-lg"/>
         <div className="absolute bottom-0 px-4 py-3 w-full">
-      <h1 className="font-semibold text-4xl"> Kittens are cute </h1>
-    </div>
+<h1 className="text-2xl font-semibold text-gray-200">Stirrups</h1>
+        </div>
+        </div>
+
         <div>
         <Image src={testPicture} alt="a test" className="h-auto max-w-full rounded-lg"/>
         </div>
@@ -53,8 +55,7 @@ const threadGallery = () => {
         <Image src={testPicture} alt="a test" className="h-auto max-w-full rounded-lg"/>
         </div>
     </div>
- </div>
-    <div/>
+</div>
     </>
   )
 }
